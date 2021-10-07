953d9cfa38362b2f367acb47862a282ff721c1cc
Isaac Obuya <Szack4477@gmail.com>
2021-10-07 13:04:37 +0300

    create and style  initial components

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from "./components/learning-resources/StoredResources.vue"
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official VueJS documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google Reference',
+          description: 'The Google site searches reference.',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  },
+  components: {
+      StoredResources,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
